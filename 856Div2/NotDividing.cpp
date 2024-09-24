05218780bd0b3b5e064fa4d1d43dfa1c6c04a268
Piyush Gupta <140248604+piyush140104@users.noreply.github.com>
2024-09-25 00:08:55 +0530

    Add files via upload

---
+++ 856Div2/NotDividing.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        int n;
+        cin>>n;
+        vector<int>arr;
+        for(int j=0;j<n;j++)
+        {
+            int ele;
+            cin>>ele;
+            arr.push_back(ele);
+        }
+        int count=0;
+        for(int w=0;w<n;w++)
+        {
+           if(w)
+        }
+        for(int j=0;j<n;j++)
+        {
+            cout<<arr[j]<<" ";
+        }
+        cout<<endl;
+    }
+
+}
